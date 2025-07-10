0fb10da2ccc5a0bb07462c951ec613344935cda5
jimni <jimin7904@daum.net>
2025-07-10 16:45:54 +0900

    add ch01

---
+++ ch01/src/sub1/Hello.java
@@ -0,0 +1,23 @@
+package sub1;
+
+
+/*
+
+날짜: 2025/07/07
+이름: 이지민
+내용: 1장 JAVA 개발환경 구축 실습 
+
+
+ */
+
+
+public class Hello {
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello Wolrd!");
+		System.out.println("Hello Java");
+		
+		
+	}
+}
---
+++ ch01/src/sub2/PrintTest.java
@@ -0,0 +1,34 @@
+package sub2;
+
+/**
+ * 날짜: 2025/07/07
+ * 이름: 이지민
+ * 내용: JAVA 출력 실습
+ * 
+ */
+
+public class PrintTest {
+
+	public static void main(String[] args) {
+		
+		
+		// 기본 출력
+		System.out.println("hello java"); // 한줄복사 ctrl+alt+↓
+		System.out.println("hello java"); // 한줄삭제 ctrl+d
+		System.out.println("hello busan"); // 작업취소 ctrl+z
+		
+		
+		// 한줄 출력 
+		System.out.print("Welcome");
+		System.out.print(" JAVA");
+		
+		
+		// 개행 출력 
+		System.out.println(); // 줄바꿈
+		System.out.println("Welcome\n");
+		System.out.println("Korea");
+		
+		
+		
+	}
+}
