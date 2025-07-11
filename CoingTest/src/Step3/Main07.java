4361f45206e0f3be100dccdb9a4bdf05c926171b
jimni <jimin7904@daum.net>
2025-07-11 13:07:05 +0900

    코테 7번

--- CoingTest/src/Step3/Main07.java
+++ CoingTest/src/Step3/Main07.java
@@ -2,31 +2,37 @@ package Step3;
 
 import java.io.*;
 
-public class Main06 {
+public class Main07 {
 	public static void main(String[] args) throws IOException {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		
 		int count = Integer.parseInt(br.readLine());
-		int[] result = new int[count];
+//		System.out.println(count);
+		int[] sum = new int[count];
 		
 		
-		for(int i=0; i<count; i++) {
+		for(int i=0; i<count ; i++) {
 			String[] input = br.readLine().split(" ");
 			int a = Integer.parseInt(input[0]);
 			int b = Integer.parseInt(input[1]);
-			result[i] = a+b;
+			sum[i] = a+b;
+			
 		}
 		
-		for (int num: result) {
-			bw.write(num+"\n");
+		int result = 1;
+		
+		for(int num:sum) {
+			System.out.printf("Case #%d: %d\n",result,num);
+			result++;
+			
 		}
 		
 		bw.flush();
 		bw.close();
 		br.close();
+
 	}
 }
-
-
-
