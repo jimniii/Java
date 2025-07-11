68e0150d1ea347bad6f46178230ec7e65b4794d7
jimni <jimin7904@daum.net>
2025-07-11 12:10:20 +0900

    코테 6번 수정

--- CoingTest/src/Step3/Main06.java
+++ CoingTest/src/Step3/Main06.java
@@ -1,11 +1,25 @@
 package Step3;
 
-import java.io.BufferedReader;
+import java.util.Scanner;
 
 public class Main06 {
 	public static void main(String[] args) {
-		BufferedReader br = 
+		Scanner sc = new Scanner(System.in);
 		
+		int count = sc.nextInt();
+		int total = 0;
+		int[] result = new int[count];
+		
+		
+		for(int i =0;i<count;i++) {
+			int a = sc.nextInt(), b = sc.nextInt();
+			total = a+b;
+			result[i] = total;
+		}
+		
+		for(int num:result) {
+			System.out.println(num);
+		}
 		
 		
 	}
